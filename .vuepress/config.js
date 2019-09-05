ea0f3450baad459fd3ef652bd285689e06393a38
youngpirate32 <youngpirate32@gmail.com>
2019-09-05 23:29:41 +0300

    docs

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,3 +1,13 @@
 module.exports = {
     base: '/docs/',
+    title: 'Ss3t',
+    description: 'Документация',
+    themeConfig: {
+        nav: [
+            { text: 'Главная', link: '/' },
+            { text: 'Быстрый старт', link: '/quick-start/' },
+            // { text: 'Погружение', link: '/dive-into/' },
+            { text: 'Discord', link: 'https://discord.gg/nBkFGnx' },
+        ]
+    }
 };
---
+++ dive-into/README.md
@@ -0,0 +1,9 @@
+---
+title: Погружение
+sidebar: auto
+sidebarDepth: 5
+---
+
+# Погружение 
+
+## todo
